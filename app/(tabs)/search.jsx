06e5ffd5620eeee218e58f6804fe2bf156783bb1
N-kapila <nirmalkapilarathne1998@gmail.com>
2025-01-29 21:51:09 +0530

    create search page

--- app/(tabs)/search.jsx
+++ app/(tabs)/search.jsx
@@ -1,135 +1,80 @@
-import { StyleSheet, Image, Platform,Pressable,View } from 'react-native';
-import { useContext } from 'react';
-import { ThemeContext } from '@/context/ThemeContext';
+import React from "react";
+import { View, TextInput, FlatList, Image, StyleSheet,TouchableOpacity } from "react-native";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import Octicons from '@expo/vector-icons/Octicons'
+const images = [
+  require("@/assets/images/search/image1.png"),
+  require("@/assets/images/search/image2.png"),
+  require("@/assets/images/search/image3.png"),
+  require("@/assets/images/search/image4.png"),
+  require("@/assets/images/search/image5.png"),
+  require("@/assets/images/search/image6.png"),
+  require("@/assets/images/search/image7.png"),
+  require("@/assets/images/search/image8.png"),
+];
 
-export default function TabTwoScreen() {
-  const { colorScheme, setColorScheme, theme } = useContext(ThemeContext);
-    const styles = createStyles(theme, colorScheme);
+const Search = () => {
+  const handleSearch = () => {
+    console.log("Search button pressed!");
+    // Add search functionality here
+  };
   return (
-    <View style={styles.container}
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Search</ThemedText>
-      </ThemedView>
-       <Pressable
-        onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')}
-        style={{ marginLeft: 10 }}
-      >
-        <Octicons
-          name={colorScheme === 'dark' ? 'moon' : 'sun'}
-          size={36}
-          color={theme.text}
-          style={{ width: 36 }}
-        />
-      </Pressable>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.jsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/search.jsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
+    <View style={styles.container}>
+     
+      <View style={styles.searchContainer}>
+        <TextInput placeholder="Search your dream location here" placeholderTextColor="#aaa" style={styles.searchInput} />
+       <TouchableOpacity onPress={handleSearch} style={styles.iconButton}>
+          <FontAwesome5 name="search-location" size={20} color="#333" />
+        </TouchableOpacity>
+      </View>
+
+     
+      <FlatList
+        data={images}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2} // 2 columns
+        renderItem={({ item }) => <Image source={item} style={styles.image} />}
+        contentContainerStyle={styles.imageGrid}
+      />
     </View>
   );
-}
+};
 
-const createStyles = (theme, colorScheme) => StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: theme.background,
+    backgroundColor: "#000", // Black background
+    paddingTop: 40,
+    marginTop: 20,
+    alignItems: "center",
   },
-  text: {
-    color: theme.text,
-    marginTop: 10,
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    width: "80%",
+    height: 40,
+    marginBottom: 20,
   },
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  searchInput: {
+    flex: 1,
+    color: "#333",
+    fontSize: 14,
   },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+   iconButton: {
+    padding: 5, 
+  },
+  imageGrid: {
+    alignItems: "center",
+  },
+  image: {
+    width: 180,
+    height: 180,
+    margin: 10,
+    borderRadius: 10,
   },
 });
+
+export default Search;
